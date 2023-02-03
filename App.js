9b8571586aa0b2e1108946c13797a094da24ff91
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-02-02 18:34:24 -0600

    get main bridge working

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet, Text, View, NativeModules, Button } from "react-native";
 
 export default function App() {
@@ -9,7 +9,52 @@ export default function App() {
       <StatusBar style="auto" />
       <Button
         title="Test"
-        onPress={() => NativeModules.MwaWalletLibModule.tryTest("Friend")}
+        onPress={() => NativeModules.MwaWalletLibModule.tryTest2("Friend")}
+      />
+      <Button
+        title="Create Scenario"
+        onPress={() => {
+          NativeModules.MwaWalletLibModule.createScenario(
+            "Backpack", // this'll always be Backpack
+            "solana-wallet:/v1/associate/local?association=BC0IWdfk8xzpyI2hum5sLWQeBOjdmp4sa7JBviKeYb4OF5w464L5ZHjk3OYhDTOfzkSlaPOJta9FjoGhsRgIFfc&port=49319", // this'll be handled by the IntentFilter and Deep Link and/or App Link
+            (event, data) => {
+              switch (event) {
+                case "SCENARIO_READY":
+                  console.log("SCENARIO_READY");
+                  break;
+                case "SCENARIO_COMPLETE":
+                  console.log("SCENARIO_COMPLETE", data);
+                  break;
+                case "SCENARIO_ERROR":
+                  console.log("SCENARIO_ERROR", data);
+                  break;
+                case "SCENARIO_TEARDOWN_COMPLETE":
+                  console.log("SCENARIO_TEARDOWN_COMPLETE", data);
+                  break;
+                case "AUTHORIZE_REQUEST":
+                  console.log("AUTHORIZE_REQUEST", data);
+                  break;
+                case "RE_AUTHORIZE_REQUEST":
+                  console.log("RE_AUTHORIZE_REQUEST", data);
+                  break;
+                case "SIGN_TRANSACTION_REQUEST":
+                  console.log("SIGN_TRANSACTION_REQUEST", data);
+                  break;
+                case "SIGN_MESSAGE_REQUEST":
+                  console.log("SIGN_MESSAGE_REQUEST", data);
+                  break;
+                case "SIGN_AND_SEND_TRANSACTION_REQUEST":
+                  console.log("SIGN_AND_SEND_TRANSACTION_REQUEST", data);
+                  break;
+                case "DE_AUTHORIZE_EVENT":
+                  console.log("DE_AUTHORIZE_EVENT", data);
+                  break;
+                default:
+                  console.log("error");
+              }
+            }
+          );
+        }}
       />
     </View>
   );
--- android/app/src/main/java/com/backpackmwa/MwaWalletLibModule.kt
+++ android/app/src/main/java/com/backpackmwa/MwaWalletLibModule.kt
@@ -1,15 +1,143 @@
 package com.backpackmwa
-import com.facebook.react.bridge.NativeModule
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
+
+import android.app.Application
+import android.content.Context
+import android.net.Uri
 import android.util.Log
+import com.facebook.react.bridge.*
+import com.solana.mobilewalletadapter.walletlib.association.AssociationUri
+import com.solana.mobilewalletadapter.walletlib.association.LocalAssociationUri
+import com.solana.mobilewalletadapter.walletlib.authorization.AuthIssuerConfig
+import com.solana.mobilewalletadapter.walletlib.protocol.MobileWalletAdapterConfig
+import com.solana.mobilewalletadapter.walletlib.scenario.*
 
-class MwaWalletLibModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+// by typing `val` we're holding onto a reference to reactContext
+class MwaWalletLibModule(val reactContext: ReactApplicationContext) :
+    ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "MwaWalletLibModule"
+    private var scenario: Scenario? = null
 
-    @ReactMethod fun tryTest(name: String) {
-        Log.d("MwaWalletLibModule", "Testing: $name");
+    companion object {
+        private val TAG = MwaWalletLibModule::class.simpleName
+    }
+
+    @ReactMethod
+    fun tryTest(name: String) {
+        Log.d(TAG, "Testing: $name")
+    }
+
+    @ReactMethod
+    fun tryTest2(name: String) {
+        Log.d(TAG, "Testing2: $name")
+    }
+
+    @ReactMethod
+    fun createScenario(
+        walletName: String, // our wallet's name (Backpack)
+        uriStr: String,
+        callback: Callback
+//        promise: Promise
+    ) {
+        // Not production code! Testing purposes only START
+        scenario?.let {
+            it.close()
+        }
+
+        scenario = null
+        // Not production code! Testing purposes only END
+
+        val uri = Uri.parse(uriStr)
+
+        val associationUri = AssociationUri.parse(uri)
+        if (associationUri == null) {
+//            promise.reject("Unsupported association URI") // update to new version
+            return
+        } else if (associationUri !is LocalAssociationUri) {
+//            promise.reject("Current implementation of fakewallet does not support remote clients")
+            return
+        }
+
+        val callbacks = createMobileWalletAdapterScenarioCallbacks(callback)
+
+        // created a scenario, told it to start (kicks off some threads in the background)
+        // we've kept a reference to it in the global state of this module (scenario)
+        // this won't be garbage collected and will just run, sit & wait for an incoming connection
+        scenario = associationUri.createScenario(
+            reactContext,
+            MobileWalletAdapterConfig(
+                true,
+                10,
+                10,
+                arrayOf(MobileWalletAdapterConfig.LEGACY_TRANSACTION_VERSION, 0)
+            ),
+            AuthIssuerConfig(walletName),
+            callbacks
+        ).also { it.start() }
+
+//        promise.resolve(true)
+    }
+
+    private fun createMobileWalletAdapterScenarioCallbacks(
+        callback: Callback
+    ): Scenario.Callbacks {
+        return object : Scenario.Callbacks {
+            // done
+            override fun onScenarioReady() {
+                Log.d(TAG, "onScenarioReady")
+                callback.invoke("SCENARIO_READY")
+            }
+
+            // done
+            override fun onScenarioServingClients() {
+                callback.invoke("SCENARIO_SERVING_CLIENTS")
+//               TODO("Not yet implemented")
+            }
+
+            // done (TBD on scenario?.close)
+            override fun onScenarioServingComplete() {
+                scenario?.close(); // returns (effectively) immediately, it doesn't take very long to run
+                callback.invoke("SERVING_COMPLETE")
+            }
+
+            // done
+            override fun onScenarioComplete() {
+                callback.invoke("SCENARIO_COMPLETE")
+            }
+
+            // done
+            override fun onScenarioError() {
+                callback.invoke("SCENARIO_ERROR")
+            }
+
+            // done (slightly different on native side but that's bc of viewModelScope)
+            override fun onScenarioTeardownComplete() {
+                callback.invoke("SCENARIO_TEARDOWN_COMPLETE")
+            }
+
+            override fun onAuthorizeRequest(request: AuthorizeRequest) {
+                callback.invoke("AUTHORIZE_REQUEST", request)
+            }
+
+            override fun onReauthorizeRequest(request: ReauthorizeRequest) {
+                callback.invoke("RE_AUTHORIZE_REQUEST", request)
+            }
+
+            override fun onSignTransactionsRequest(request: SignTransactionsRequest) {
+                callback.invoke("SIGN_TRANSACTION_REQUEST", request)
+            }
+
+            override fun onSignMessagesRequest(request: SignMessagesRequest) {
+                callback.invoke("SIGN_MESSAGE_REQUEST", request)
+            }
+
+            override fun onSignAndSendTransactionsRequest(request: SignAndSendTransactionsRequest) {
+                callback.invoke("SIGN_AND_SEND_TRANSACTION_REQUEST", request)
+            }
+
+            override fun onDeauthorizedEvent(event: DeauthorizedEvent) {
+                callback.invoke("DE_AUTHORIZE_EVENT", event)
+            }
+
+        }
     }
 }
